c49acdec6c4550d4a6c1192267e04426cd2a6b78
dimiturstefanow <dimitrstefanov7v2@gmail.com>
2023-07-14 15:53:22 +0300

    darkmode

--- src/components/HeaderDropDown.jsx
+++ src/components/HeaderDropDown.jsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import boardIcon from "../assets/icon-board.svg";
+import darkIcon from "../assets/icon-dark-theme.svg";
+import lightIcon from "../assets/icon-light-theme.svg";
+import { Switch } from "@headlessui/react";
+import useDarkMode from "../Hooks/useDarkMode";
 
 function HeaderDropDown({ setOpenDropdown }) {
-  const boards = useSelector((state) => state.boards);
+  const [colorTheme, setTheme] = useDarkMode();
+  const [darkSide, setDarkSide] = useState(
+    colorTheme === "light" ? true : false
+  );
 
-  console.log(" boards = ", boards);
+  const toggleDarkMode = (checked) => {
+    setTheme(colorTheme);
+    setDarkSide(checked);
+  };
+
+  const boards = useSelector((state) => state.boardsSlice);
 
   return (
     <div
@@ -37,11 +49,25 @@ function HeaderDropDown({ setOpenDropdown }) {
           ))}
 
           <div className="flex items-baseline space-x-2 text-[#635fc7] px-5 py-4">
-
             <img src={boardIcon} className=" h-4" />
-            <p className=" text-lg font-bold">
-                Create New Board
-            </p>
+            <p className=" text-lg font-bold">Create New Board</p>
+          </div>
+          <div className=" mx-2 p-4 space-x-2 bg-slate-100 dark:bg-[#20212c] flex justify-center items-center rounded=lg">
+            <img src={lightIcon} />
+            <Switch
+              checked={darkSide}
+              onChange={toggleDarkMode}
+              className={` ${darkSide ? " bg-[#635fc7]" : " bg-gray-200"}
+            relative inline-flex h-6 w-11 items-center rounded-full
+            `}
+            >
+              <span
+                className={` ${darkSide ? "translate-x-6" : " translate-x-1"}
+                inline-block h-4 w-4 transform rounded-full bg-white transition 
+                `}
+              />
+            </Switch>
+            <img src={darkIcon} />
           </div>
         </div>
       </div>
--- src/redux/boardsSlice.js
+++ src/redux/boardsSlice.js
@@ -3,7 +3,7 @@ import data from "../data/data.json"
 
 const boardsSlice = createSlice({
     name: "boards",
-    initialState : data.boards ,
+    initialState: data.boards || [] ,
     reducers : {
         // write our reducers
     }
